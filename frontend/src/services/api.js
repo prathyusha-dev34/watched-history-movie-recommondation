8be7aa0df0def6c362f692f005860e210a7d5d98
KARTHIK .S <138972829+Karthiks2202@users.noreply.github.com>
2026-06-23 08:18:39 +0530

    Update backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 // Base API URL
 const API = axios.create({
   baseURL: "http://127.0.0.1:8000",
+   baseURL: "https://collect-dd4h.onrender.com",
 });
 
 // Add token automatically
